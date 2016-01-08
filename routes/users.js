4c656ac03b3753612db7dfae3c04d5170bf37d90
zhuhuijun <zhuhjtcx@qq.com>
2016-01-08 23:04:11 +0800

    user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var crypto = require('crypto');
+var myhelper = require('../MyHelper');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
@@ -9,16 +10,14 @@ router.get('/', function (req, res, next) {
 
 router.post('/reg', function (req, res, next) {
     var user = req.body;
-    var md5 = crypto.createHash('md5');
-    console.info(user);
-    var md5Email = md5.update(user.uemail.toLowerCase()).digest('hex');
+    var md5Email = myhelper.Md5Helper(user.uemail);
     var avatar = "https://secure.gravatar.com/avatar/" + md5Email + "?s=48";
     console.info(avatar);
     new models.User({
-        username: 'zhuhj',
-        upwd: 'pwd',
-        uemail: 'email',
-        avator: 'avatar'
+        username: user.username,
+        upwd: myhelper.Md5Helper(user.upwd),
+        uemail: user.uemail,
+        avator: avatar
     }).save(function (err, re) {
             if (err) {
                 res.json(500, {msg: err});
